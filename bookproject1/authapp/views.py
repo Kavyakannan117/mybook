dc8e8f172fc1ca4444f415711ec22e6333f76821
Kavya Kannan <131740949+Kavyakannan117@users.noreply.github.com>
2024-04-30 14:21:40 +0530

    Update views.py

--- bookproject1/authapp/views.py
+++ bookproject1/authapp/views.py
@@ -1,75 +1,56 @@
 from django.contrib import messages,auth
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from .models import loginTable, UserProfile
 # Create your views here.
 
-
-def userRegistration(request):
-    login_table=loginTable()
-    userprofile=UserProfile()
-
+def RegisterUser(request):
     if request.method=='POST':
-        userprofile.username=request.POST['username']
-        userprofile.password=request.POST['password']
-        userprofile.password2=request.POST['password1']
-
-        login_table.username=request.POST['username']
-        login_table.password=request.POST['password']
-        login_table.password2=request.POST['password1']
-        login_table.type='user'
-
-        if request.POST['password']==request.POST['password1']:
-            userprofile.save()
-            login_table.save()
-
-            messages.info(request,'Registration Success')
+        username=request.POST.get('username')
+        firstname=request.POST.get('first_name')
+        lastname=request.POST.get('last_name')
+        email=request.POST.get('email')
+        password=request.POST.get('password')
+        cpassword=request.POST.get('password1')
+        if password==cpassword:
+            if User.objects.filter(username=username).exists():
+                messages.info(request,'This user already exist')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request,'email already exists')
+                return redirect('register')
+            else:
+                 user=User.objects.create_user(username=username,first_name=firstname,last_name=lastname,
+                                           email=email,password=password)
+            user.save()
             return redirect('login')
         else:
-            messages.info(request,'Password is not matching')
+            messages.info(request,'This password is not matching')
             return redirect('register')
     return render(request,'auth/registration.html')
 
-
-
-def Login(request):
-    if request.method=='POST':
-        username=request.POST['username']
-        password=request.POST['password']
-        user=loginTable.objects.filter(username=username,password=password,type='user').exists()
-        print(user)
-
-        try:
+def LoginUser(request):
+        if request.method=='POST':
+            username=request.POST.get('username')
+            password=request.POST.get('password')
+            user=auth.authenticate(username=username,password=password)
             if user is not None:
-                user_details=loginTable.objects.get(username=username,password=password)
-                user_name=user_details.username
-                type=user_details.type
-
-                if type=='user':
-                    request.session['username']=user_name
-                    return redirect('user_view')
-                elif type=='admin':
-                    request.session['username']=user_name
-                    return redirect('admin_view')
-
+                auth.login(request,user)
+                return redirect('createbook')
             else:
-                messages.info(request,'invalid username or password')
+                messages.info(request,'Please provide correct details')
+                return redirect('login')
+        return render(request,'auth/login.html')
 
-        except:
-            messages.info(request,'invalid credentials')
-
-    return render(request,'auth/login.html')
 
 @login_required
 def admin_view(request):
-    user_name=request.session.get('username',None)
-    return render(request,'auth/admin_view.html',{'user_name':user_name})
 
-def user_view(request):
-    user_name=request.session.get('username')
-    return render(request,'auth/user_view.html',{'user_name':user_name})
+    return render(request,'admin/book.html')
+
 
 def log_out(request):
-    logout(request)
+    auth.logout(request)
     return redirect('login')
