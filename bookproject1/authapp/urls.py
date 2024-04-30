13d7be3edc26586b501c4e470fc6e15a6597037f
Kavya Kannan <131740949+Kavyakannan117@users.noreply.github.com>
2024-04-30 14:23:22 +0530

    Update urls.py

--- bookproject1/authapp/urls.py
+++ bookproject1/authapp/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("register/",views.userRegistration,name='register'),
-    path('',views.Login,name="login"),
+    path("register/",views.RegisterUser,name='register'),
+    path('',views.LoginUser,name="login"),
     path('adminview/',views.admin_view,name="admin_view"),
-    path('userlogin/',views.user_view,name='user_view'),
+  
     path('logout/',views.log_out,name='logout'),
 
 ]
